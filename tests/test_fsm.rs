30277658ce2947f7619e1e13abf2f2aaed990c08
Celezt <celezt@github.com>
2020-11-28 19:38:19 +0100

    "added test"

---
+++ tests/test_fsm.rs
@@ -0,0 +1,38 @@
+#[cfg(test)]
+mod tests {
+    use cfsm::FSM;
+
+    #[derive(Debug, Copy, Clone)]
+    struct TestState {
+        x: i32,
+    }
+
+    /// For test purposes
+    impl TestState {
+        fn a(&mut self) -> FSM<TestState> {
+            self.x += 1;
+            println!("A: {:?}", &self);
+            FSM::transition(*self, Self::b)
+        }
+        fn b(&mut self) -> FSM<TestState> {
+            self.x += 1;
+            println!("B: {:?}", &self);
+            FSM::transition(*self, Self::c)
+        }
+        fn c(&mut self) -> FSM<TestState> {
+            self.x += 1;
+            println!("C: {:?}", &self);
+            FSM::transition(*self, Self::a)
+        }
+    }
+
+    #[test]
+    fn test_fsm() {
+        // Initialize
+        let mut state = FSM::transition(TestState { x: 0 }, TestState::a);
+
+        for _ in 0..20 {
+            state.update();
+        }
+    }
+}
